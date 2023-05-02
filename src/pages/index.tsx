201b8bf43d297a5beee4b68b16cb6e649f49167a
John Nguyen <nguyenduyjohn180@gmail.com>
2023-05-02 01:26:28 -0700

    finished youtube tutorial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,30 +21,6 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
@@ -155,3 +131,108 @@ const AuthShowcase: React.FC = () => {
     </div>
   );
 };
+
+// THE REST OF THE STUFF FROM THE TUTORIAL WHICH HAS DAISYUI CODE
+
+// type Topic = RouterOutputs["topic"]["getAll"][0];
+
+// const Content: React.FC = () => {
+//   const { data: sessionData } = useSession();
+
+//   const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
+
+//   const { data: topics, refetch: refetchTopics } = api.topic.getAll.useQuery(
+//     undefined, // no input
+//     {
+//       enabled: sessionData?.user !== undefined,
+//       onSuccess: (data) => {
+//         setSelectedTopic(selectedTopic ?? data[0] ?? null);
+//       },
+//     }
+//   );
+
+//   const createTopic = api.topic.create.useMutation({
+//     onSuccess: () => {
+//       void refetchTopics();
+//     },
+//   });
+
+//   const { data: notes, refetch: refetchNotes } = api.note.getAll.useQuery(
+//     {
+//       topicId: selectedTopic?.id ?? "",
+//     },
+//     {
+//       enabled: sessionData?.user !== undefined && selectedTopic !== null,
+//     }
+//   );
+
+//   const createNote = api.note.create.useMutation({
+//     onSuccess: () => {
+//       void refetchNotes();
+//     },
+//   });
+
+//   const deleteNote = api.note.delete.useMutation({
+//     onSuccess: () => {
+//       void refetchNotes();
+//     },
+//   });
+
+//   return (
+//     <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+//       <div className="px-2">
+//         <ul className="menu rounded-box w-56 bg-base-100 p-2">
+//           {topics?.map((topic) => (
+//             <li key={topic.id}>
+//               <a
+//                 href="#"
+//                 onClick={(evt) => {
+//                   evt.preventDefault();
+//                   setSelectedTopic(topic);
+//                 }}
+//               >
+//                 {topic.title}
+//               </a>
+//             </li>
+//           ))}
+//         </ul>
+//         <div className="divider"></div>
+//         <input
+//           type="text"
+//           placeholder="New Topic"
+//           className="input-bordered input input-sm w-full"
+//           onKeyDown={(e) => {
+//             if (e.key === "Enter") {
+//               createTopic.mutate({
+//                 title: e.currentTarget.value,
+//               });
+//               e.currentTarget.value = "";
+//             }
+//           }}
+//         />
+//       </div>
+//       <div className="col-span-3">
+//         <div>
+//           {notes?.map((note) => (
+//             <div key={note.id} className="mt-5">
+//               <NoteCard
+//                 note={note}
+//                 onDelete={() => void deleteNote.mutate({ id: note.id })}
+//               />
+//             </div>
+//           ))}
+//         </div>
+
+//         <NoteEditor
+//           onSave={({ title, content }) => {
+//             void createNote.mutate({
+//               title,
+//               content,
+//               topicId: selectedTopic?.id ?? "",
+//             });
+//           }}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
